f80fddd37fa3a62e4b0848a23c53e109840590fe
mario-mitchell <35703472+mario-mitchell@users.noreply.github.com>
2022-09-19 09:07:55 -0700

    Adding more colors for selection

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ if (choice === "construct") {
         console.log("Red and Yellow together to make Orange")
     } else if ((color1 === "blue" && color2 === "yellow") || (color1 === "yellow" && color2 === "blue")) {
         console.log("Blue and Yellow together to make Green")
+    } else if ((color1 === "red" && color2 === "green") || (color1 === "green" && color2 === "red")) {
+        console.log("Red and Green together to make Brown")
+    } else if ((color1 === "blue" && color2 === "green") || (color1 === "green" && color2 === "blue")) {
+        console.log("Blue and Green together to make Blue-Green")
+    } else if ((color1 === "red" && color2 === "white") || (color1 === "white" && color2 === "red")) {
+        console.log("Red and White together to make Pink")
+    } else if ((color1 === "White" && color2 === "Black") || (color1 === "Black" && color2 === "White")) {
+        console.log("Black and White together to make Gray")
+    } else if ((color1 === "Yellow" && color2 === "Red") || (color1 === "Red" && color2 === "Yellow")) {
+        console.log("Red and Yellow together to make Orange")
+    } else if ((color1 === "Blue" && color2 === "White") || (color1 === "White" && color2 === "Blue")) {
+        console.log("Blue and White together to make Light Blue")
     } else console.log("Error!")
  //Deconstructor 
 } else if (choice === "deconstruct") {
@@ -23,6 +35,18 @@ if (choice === "construct") {
         console.log("Orange deconstructed makes Red and Yellow")
     } else if (color3 === "green") {
         console.log("Green deconstructed makes Blue and Yellow")
+    } else if (color3 === "blue-green") {
+        console.log("Blue-Green deconstructed makes Blue and Green")
+    } else if (color3 === "brown") {
+        console.log("Brown deconstructed makes Red and Green")
+    } else if (color3 === "pink") {
+        console.log("Pink deconstructed makes Red and White")
+    } else if (color3 === "Gray") {
+        console.log("Gray deconstructed makes Black and White")
+    } else if (color3 === "orange") {
+        console.log("Orange deconstructed makes Red and Yellow")
+    } else if (color3 === "light blue") {
+        console.log("Light Blue deconstructed makes White and Blue")
     } else console.log("Error!")
     
 }
